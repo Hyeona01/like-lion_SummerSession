fa6f4354200b514f2bf7f5317b570cb83c49e0ee
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-07-11 20:41:28 +0900

    Feat: fakenews 추가 및 컴포넌트 분리

--- SummerSession_HW/src/App.jsx
+++ SummerSession_HW/src/App.jsx
@@ -1,45 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import Timer from './project/timer';
+import FakeNews from './project/fakenews';
 
 function App() {
-  const timeSet = () => {
-    const now = new Date();
-    const day = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59);
-    const timeDiff = Math.floor((day - now) / 1000);
-
-    const hours = Math.floor(timeDiff / 3600);
-    const minutes = Math.floor((timeDiff % 3600) / 60);
-    const seconds = timeDiff % 60;
-
-    return { hours, minutes, seconds };
-  };
-
-  const [time, setTime] = useState(timeSet());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(timeSet());
-    }, 1000); 
-
-    const consoleInterval = setInterval(() => {
-      console.log("1분이 지남");
-    }, 60000);
-
-    return () => {
-      clearInterval(interval);
-      clearInterval(consoleInterval);
-    };
-  }, []);
-
   return (
-    <div>
-      <p>오늘이</p>
-      <h2>
-        {time.hours.toString().padStart(2, '0')}:
-        {time.minutes.toString().padStart(2, '0')}:
-        {time.seconds.toString().padStart(2, '0')}
-      </h2>
-      <p>밖에 안남았다니..</p>
-    </div>
+    <>
+    <Timer/>
+    <hr/>
+    <FakeNews/>
+    </>
   );
 }
 
